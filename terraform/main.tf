969a0ece3557b6e89aaf5692cc33ff1f296c10ba
Yuki Yoshida <yuyoshd@amazon.co.jp>
2023-10-02 22:44:33 +0900

    add 84

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,31 @@ module "yuyoshd83" {
 
   account_customizations_name = "sandbox"
 }
+
+module "yuyoshd84" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "yuyoshd+84@amazon.co.jp"
+    AccountName               = "yuyoshd84"
+    ManagedOrganizationalUnit = "hoge"
+    SSOUserEmail              = "yuyoshd@amazon.co.jp"
+    SSOUserFirstName          = "aft"
+    SSOUserLastName           = "test"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
